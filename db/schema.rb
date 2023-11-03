6dc4ccede15a084616f6cf0adad5baf835df2c46
YuhriParada <yuhriparada@gmail.com>
2023-11-02 21:52:31 -0300

    reviews table migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_03_004434) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_03_005154) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,18 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_03_004434) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_recovery_codes_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.integer "rating"
+    t.bigint "user_id", null: false
+    t.bigint "property_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["property_id"], name: "index_reviews_on_property_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "sessions", force: :cascade do |t|
@@ -86,6 +98,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_03_004434) do
 
   add_foreign_key "property_addresses", "properties"
   add_foreign_key "recovery_codes", "users"
+  add_foreign_key "reviews", "properties"
+  add_foreign_key "reviews", "users"
   add_foreign_key "sessions", "users"
   add_foreign_key "user_addresses", "users"
 end
